247ead736bf6bdd33fad526b1c8b2f25e8cc4b34
Rasmus-2 <grasmus95@gmail.com>
2024-01-09 15:38:28 +0100

    Shoppingcart
    View shoppingcart klart typ!

--- GroupBWebshop/CustomerView.cs
+++ GroupBWebshop/CustomerView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,7 +36,8 @@ namespace GroupBWebshop
                 Window display = new Window("", 2, 5, productDisplay.ToList());
                 display.Draw();
 
-                List<string> cart = new List<string>() { "empty" };
+                var cart = CartView(customerId);
+
                 Window box = new Window("Cart", 55, 1, cart);
                 box.Draw();
 
@@ -45,6 +47,7 @@ namespace GroupBWebshop
                 Console.WriteLine("3. View account information");
                 Console.WriteLine("4. View history");
                 Console.WriteLine("5. Search");
+                Console.WriteLine("6. Log out");
 
                 var key = Console.ReadKey();
 
@@ -79,35 +82,17 @@ namespace GroupBWebshop
                                 Console.WriteLine("Enter quantity");
                                 int quantity = int.Parse(Console.ReadLine());
 
-                                //var existingOrder = (from o in myDb.Orders
-                                //                     where o.CustomerId == customerId && !o.Completed
-                                //                     select o).SingleOrDefault();
-
                                 var existingOrder = GetOrderId(customerId);
 
                                 if (existingOrder == null)
                                 {
                                     myDb.Add(new Order { CustomerId = customerId, Completed = false });
+                                    myDb.SaveChanges();
                                 }
                                 var getOrderId = GetOrderId(customerId);
 
                                 OrderDetails orderDetails = new OrderDetails() { OrderId = getOrderId.Id, ProductId = inputId, Quantity = quantity };
                                 myDb.Add(orderDetails);
-
-                                /*   var getOrder = (from o in myDb.Orders
-                                                   where o.CustomerId == customerId && !o.Completed
-                                                   select o.Id).SingleOrDefault();
-
-                                   OrderDetails orderDetails = new OrderDetails() { OrderId = getOrder, ProductId = inputId, Quantity = quantity };
-                                   myDb.Add(orderDetails);
-                               }
-                               else
-                               {
-                                   OrderDetails orderDetails = new OrderDetails() { OrderId = test, ProductId = inputId, Quantity = quantity };
-                                   myDb.Add(orderDetails);
-                               }*/
-
-
 
                                 myDb.SaveChanges();
                             }
@@ -116,13 +101,42 @@ namespace GroupBWebshop
                         break;
 
                     case '2':
-                        Console.WriteLine("");
+                        Console.Clear();
+                        var finalCart = CartView(customerId);
+                        var total = CartPrice(customerId);
+                        finalCart.Add("");
+                        finalCart.Add("Total sum: " + total + " SEK");
+                        Window cartBox = new Window("Your cart", 1, 1, finalCart);
+                        cartBox.Draw();
+                        Console.WriteLine("1. Continue to payment");
+                        Console.WriteLine("2. Continue shopping");
+                        var exitCart = Console.ReadKey();
+                        if(exitCart.KeyChar == '1')
+                        {
+                            Console.WriteLine("payment methods");
+                            foreach (var i in Enum.GetValues(typeof(MyEnums.PaymentMethod)))
+                            {
+                                Console.WriteLine(Enum.GetName(typeof(MyEnums.PaymentMethod), i).Replace('_', ' '));
+                            }
+
+                        }
+                        else if(exitCart.KeyChar == '2')
+                        {
+                            Console.WriteLine("Shopping!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Try again loser");
+                        }
                         break;
+
                     case '3':
                         break;
                     case '4':
                         break;
                     case '5':
+                        break;
+                    case '6':
                         break;
                 }
 
@@ -130,6 +144,63 @@ namespace GroupBWebshop
             }
 
 
+        }
+
+        public static double CartPrice(int customerId)
+        {
+            double total = 0;
+
+            using (var myDb = new MyDbContext())
+            {
+                var orderId = GetOrderId(customerId);
+
+                var productsInCart = (from o in myDb.OrderDetails
+                                      where o.OrderId == orderId.Id
+                                      select o.ProductId).ToList();
+
+                foreach (var product in productsInCart)
+                {
+                    var quantityInCart = (from o in myDb.OrderDetails
+                                          where o.ProductId == product
+                                          && o.OrderId == orderId.Id
+                                          select o.Quantity).FirstOrDefault();
+
+                    var productInfo = (from p in myDb.Products
+                                       where p.Id == product
+                                       select p).SingleOrDefault();
+
+                    total += quantityInCart * productInfo.Price;
+                }
+            }
+            return total;
+        }
+
+        public static List<string> CartView(int customerId)
+        {
+            List<string> cart = new List<string>() { };
+
+            using (var myDb = new MyDbContext())
+            {
+                var orderId = GetOrderId(customerId);
+
+                var productsInCart = (from o in myDb.OrderDetails
+                                      where o.OrderId == orderId.Id
+                                      select o.ProductId).ToList();
+
+                foreach (var product in productsInCart)
+                {
+                    var quantityInCart = (from o in myDb.OrderDetails
+                                          where o.ProductId == product
+                                          && o.OrderId == orderId.Id
+                                          select o.Quantity).FirstOrDefault();
+
+                    var productInfo = (from p in myDb.Products
+                                       where p.Id == product
+                                       select p).SingleOrDefault();
+                    cart.Add(productInfo.Name + ", antal: " + quantityInCart + ", " + productInfo.Price + " SEK styck. Totalt: " + (quantityInCart * productInfo.Price) + " SEK");
+                }
+            }
+            return cart;
         }
 
         public static Order GetOrderId(int customerId)
